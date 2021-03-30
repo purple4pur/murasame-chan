6ed440458dec6ee8305ba780d8dc2b74c9320ab4
Purple4pur <purple4pur@gmail.com>
2021-03-30 10:48:49 +0800

    init

---
+++ bot.py
@@ -0,0 +1,15 @@
+import nonebot
+from nonebot.adapters.cqhttp import Bot as CQHTTPBot
+
+nonebot.init()
+
+driver = nonebot.get_driver()
+driver.register_adapter("cqhttp", CQHTTPBot)
+nonebot.load_builtin_plugins()
+
+# app = nonebot.get_asgi()
+
+nonebot.load_plugins("src/plugins")
+
+if __name__ == "__main__":
+    nonebot.run()

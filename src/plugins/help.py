f68a77321d98871b40e177b76788298201fb2e4b
Purple4pur <purple4pur@gmail.com>
2021-04-02 15:40:34 +0800

    fix error

--- src/plugins/help.py
+++ src/plugins/help.py
@@ -1,10 +1,11 @@
 from nonebot import on_command
+from nonebot.adapters import Bot
 
 
 help = on_command("help", priority=5)
 
 @help.handle()
-async def handle():
+async def handle(bot: Bot):
     await help.finish("""小从雨现在可以
 /help          : 显示此帮助菜单
 /ping          : 看看一下小从雨在不在
--- src/plugins/ping.py
+++ src/plugins/ping.py
@@ -1,8 +1,9 @@
 from nonebot import on_command
+from nonebot.adapters import Bot
 
 
 ping = on_command("ping", priority=5)
 
 @ping.handle()
-async def handle():
+async def handle(bot: Bot):
     await ping.finish("ムラサメです！")

22779ec70d209d2c19efc52d046ae812722f7e86
Purple4pur <purple4pur@gmail.com>
2021-04-05 20:27:14 +0800

    new module: artificial_idiot.py

--- src/plugins/help.py
+++ src/plugins/help.py
@@ -10,4 +10,5 @@ async def handle(bot: Bot):
 /help : 呃啊
 /ping : 歪？是小从雨吗？
 /roll <number=100> : 在 [0, number] 里摇点
-/roll [arg1] [arg2] <...> : 专治选择困难""")
+/roll [arg1] [arg2] <...> : 专治选择困难
+/[...是不是/要不要...] : 快跑！人工智障！""")

4bab6a50fabbf0546877d888adbb4a011dc318ae
Purple4pur <purple4pur@gmail.com>
2021-04-06 13:02:59 +0800

    sleep on dev

--- src/plugins/sleep-dev.py
+++ src/plugins/sleep-dev.py
@@ -2,9 +2,12 @@ from nonebot import on_command
 from nonebot.adapters import Bot
 from nonebot.adapters.cqhttp import MessageSegment
 
+from os.path import dirname, abspath
+
 
 sleep = on_command("sleep", priority=1, block=True)
 
 @sleep.handle()
 async def handle(bot: Bot):
-    await sleep.finish(MessageSegment.record("file:///..\\static\\audio\\你怎么睡得着的.mp3"))
+    path = dirname(abspath(__file__))
+    await sleep.finish(MessageSegment.record(f"file:///{path}\\..\\static\\audio\\你怎么睡得着的.mp3"))

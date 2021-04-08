9f4de9720d862b18175b128f279e3055a2ccae40
Purple4pur <purple4pur@gmail.com>
2021-04-08 23:37:37 +0800

    improve contact msg

--- src/plugins/contact.py
+++ src/plugins/contact.py
@@ -9,9 +9,19 @@ contact = on_command("contact", aliases={"feedback"}, priority=1, block=True)
 
 @contact.handle()
 async def handle(bot: Bot, event: MessageEvent):
+    is_from_private = True
+    session_id = event.get_session_id().split("_")
+    if len(session_id) == 3:
+        is_from_private = False
+        group_id = session_id[1]
+    
     content = str(event.get_message()).strip()
     now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    msg = f"Sender: {event.get_user_id()}\nTime: {now}\nContent: {content}"
+
+    if is_from_private:
+        msg = f"[Contact Message]\nSender: {event.get_user_id()}\nFrom: Private\nTime: {now}\nContent: {content}"
+    else:
+        msg = f"[Contact Message]\nSender: {event.get_user_id()}\nFrom: Group({group_id})\nTime: {now}\nContent: {content}"
     await bot.send_private_msg(user_id=593457446, message=msg)
 
     await contact.finish("小从雨已经向狗修金报告啦~")

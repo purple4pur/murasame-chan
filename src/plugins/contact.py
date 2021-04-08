f5a0e38d4a7c3b68f02fd54ddb2fa7bc340b5105
Purple4pur <purple4pur@gmail.com>
2021-04-08 23:50:14 +0800

    update README & improve contact

--- src/plugins/contact.py
+++ src/plugins/contact.py
@@ -5,23 +5,27 @@ from nonebot.adapters.cqhttp import MessageEvent, MessageSegment
 from datetime import datetime
 
 
-contact = on_command("contact", aliases={"feedback"}, priority=1, block=True)
+contact = on_command("contact", aliases={"feedback", "反馈"}, priority=1, block=True)
 
 @contact.handle()
 async def handle(bot: Bot, event: MessageEvent):
-    is_from_private = True
-    session_id = event.get_session_id().split("_")
-    if len(session_id) == 3:
-        is_from_private = False
-        group_id = session_id[1]
-    
     content = str(event.get_message()).strip()
-    now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-
-    if is_from_private:
-        msg = f"[Contact Message]\nSender: {event.get_user_id()}\nFrom: Private\nTime: {now}\nContent: {content}"
+    if not content:
+        await contact.finish("啊叻？小从雨不知道要发送什么啦...")
+    
     else:
-        msg = f"[Contact Message]\nSender: {event.get_user_id()}\nFrom: Group({group_id})\nTime: {now}\nContent: {content}"
-    await bot.send_private_msg(user_id=593457446, message=msg)
+        now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
-    await contact.finish("小从雨已经向狗修金报告啦~")
+        is_from_private = True
+        session_id = event.get_session_id().split("_")
+        if len(session_id) == 3:
+            is_from_private = False
+            group_id = session_id[1]
+
+        if is_from_private:
+            msg = f"[Contact Message]\nSender: {event.get_user_id()}\nFrom: Private\nTime: {now}\nContent: {content}"
+        else:
+            msg = f"[Contact Message]\nSender: {event.get_user_id()}\nFrom: Group({group_id})\nTime: {now}\nContent: {content}"
+        await bot.send_private_msg(user_id=593457446, message=msg)
+
+        await contact.finish("小从雨已经向狗修金报告啦~")

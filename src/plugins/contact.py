935513b8c111f11ec2adba0b01508868e9b722fc
Purple4pur <purple4pur@gmail.com>
2021-04-08 23:27:52 +0800

    contact may work

--- src/plugins/contact.py
+++ src/plugins/contact.py
@@ -1,12 +1,17 @@
 from nonebot import on_command
-from nonebot.typing import T_State
 from nonebot.adapters import Bot
 from nonebot.adapters.cqhttp import MessageEvent, MessageSegment
+
+from datetime import datetime
 
 
 contact = on_command("contact", aliases={"feedback"}, priority=1, block=True)
 
 @contact.handle()
 async def handle(bot: Bot, event: MessageEvent):
-    msg = str(event.get_message()).strip()
-    bot.send_private_msg(user_id=593457446, message=msg)
+    content = str(event.get_message()).strip()
+    now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    msg = f"Sender: {event.get_user_id()}\nTime: {now}\nContent: {content}"
+    await bot.send_private_msg(user_id=593457446, message=msg)
+
+    await contact.finish("小从雨已经向狗修金报告啦~")

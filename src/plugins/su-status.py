033b7220cccadc50920b83c5f034a099804509d6
Purple4pur <purple4pur@gmail.com>
2021-04-12 11:08:19 +0800

    welcome su commands!

--- src/plugins/su-status.py
+++ src/plugins/su-status.py
@@ -10,14 +10,10 @@ status = on_command("status", permission=SUPERUSER, priority=1, block=True)
 
 @status.handle()
 async def handle(bot: Bot, event: MessageEvent):
-    friends_data = await bot.get_friend_list()
-    friends = loads(friends_data)
-    num_friends = len(friends) - 2
+    friends_data = await bot.get_friend_list() # list
+    num_friends = len(friends_data) - 2        # 排除自己和 babyQ
 
     groups_data = await bot.get_group_list()
-    groups = loads(groups_data)
-    num_groups = len(groups)
-
-    print(dumps(groups_data))
+    num_groups = len(groups_data)
 
     await status.finish(f"当前共 {num_friends} 个好友，{num_groups} 个群组。")

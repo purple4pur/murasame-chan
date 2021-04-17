8193ca1c8732126935742b220da69a1d34cff503
Purple4pur <purple4pur@gmail.com>
2021-04-17 11:12:14 +0800

    mod util

--- src/util/dump_sleep_tracker_date.py
+++ src/util/dump_sleep_tracker_date.py
@@ -1,6 +1,30 @@
 from pickle import load
-from json import dumps
 from pathlib import Path
+
+
+# https://stackoverflow.com/a/26209900
+def pretty(value, htchar="    ", lfchar='\n', indent=0):
+    nlch = lfchar + htchar * (indent + 1)
+    if type(value) is dict:
+        items = [
+            nlch + repr(key) + ': ' + pretty(value[key], htchar, lfchar, indent + 1)
+            for key in value
+        ]
+        return '{%s}' % (','.join(items) + lfchar + htchar * indent)
+    elif type(value) is list:
+        items = [
+            nlch + pretty(item, htchar, lfchar, indent + 1)
+            for item in value
+        ]
+        return '[%s]' % (','.join(items) + lfchar + htchar * indent)
+    elif type(value) is tuple:
+        items = [
+            nlch + pretty(item, htchar, lfchar, indent + 1)
+            for item in value
+        ]
+        return '(%s)' % (','.join(items) + lfchar + htchar * indent)
+    else:
+        return repr(value)
 
 
 data_path = str(Path(__file__).parent.absolute()) + "/../data/sleep_tracker_data.pickle"
@@ -9,6 +33,6 @@ try:
     f = open(data_path, "rb")
     data = load(f)
     f.close()
-    print(dumps(data, indent=4))
+    print(pretty(data))
 except FileNotFoundError:
     exit("未找到 sleep_tracker_data.pickle")

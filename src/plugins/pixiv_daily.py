6cd936050148298700f1676cf4769b0649aad504
Purple4pur <purple4pur@gmail.com>
2021-04-23 18:32:16 +0800

    test timeout in pixiv_daily

--- src/plugins/pixiv_daily.py
+++ src/plugins/pixiv_daily.py
@@ -6,8 +6,13 @@ from nonebot.permission import SUPERUSER
 
 import feedparser
 import re
+import socket
 from random import choice
 from urllib.parse import quote
+
+
+# 设置全局 timeout
+socket.setdefaulttimeout(5)
 
 
 pixiv_search = on_command("test给点", permission=SUPERUSER, priority=1, block=True)
@@ -23,32 +28,43 @@ async def handle(bot: Bot, event: MessageEvent, state: T_State):
 
     await pixiv_search.send("正在搜索[" + state["arg1"] + "]，请耐心等待...")
 
-    data = await get_image_data(keyword=state["arg1"])
-    chosen = choice(data)
-    await pixiv_search.finish(f"{chosen[0]}\n{chosen[1]}\n" + MessageSegment.image(chosen[2]))
+    is_timeout, status, data = await get_image_data(keyword=state["arg1"])
+    if not is_timeout:
+        chosen = choice(data)
+        await pixiv_search.finish(f"{chosen[0]}\n{chosen[1]}\n" + MessageSegment.image(chosen[2]))
+    else:
+        await pixiv_search.finish("苦しい……请求超时了，稍后重试一下呢")
 
 
 pixiv_daily = on_command("给点", priority=1, block=True)
 
 @pixiv_daily.handle()
 async def handle(bot: Bot, event: MessageEvent):
-    data = await get_image_data(url="https://rakuen.thec.me/PixivRss/daily-30")
+    is_timeout, status, data = await get_image_data(url="https://rakuen.thec.me/PixivRss/daily-30")
     chosen = choice(data)
     await pixiv_daily.finish(f"{chosen[0]}\n{chosen[1]}\n" + MessageSegment.image(chosen[2]))
 
 
-async def get_image_data(url: str = None, keyword: str = None) -> list:
+async def get_image_data(url: str = None, keyword: str = None) -> (bool, str, list):
+                                                            # 是否超时，状态码，data 数组
     data = []
 
     if keyword:
         url = f"https://rsshub.app/pixiv/search/{quote(keyword)}/popular/1"
 
-    rss = feedparser.parse(url)
-    for entry in rss["entries"]:
-        data.append([entry["title"], entry["link"], entry["summary"]])
+    try:
+        rss = feedparser.parse(url)
 
-    for item in data:
-        item[1] = item[1][12:]
-        item[2] = re.findall(r"src=.+?jpg", item[2])[0][5:]
+    # 请求超时
+    except socket.timeout:
+        return (True, "", data)
 
-    return data
+    else:
+        for entry in rss["entries"]:
+            data.append([entry["title"], entry["link"], entry["summary"]])
+
+        for item in data:
+            item[1] = item[1][12:]
+            item[2] = re.findall(r"src=.+?jpg", item[2])[0][5:]
+
+        return (False, rss["status"], data)

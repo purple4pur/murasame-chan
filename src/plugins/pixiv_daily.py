ab3f049b8319d8d15c339b8bcd58cd2e1c86a8e9
Purple4pur <purple4pur@gmail.com>
2021-04-28 14:27:10 +0800

    pixiv_daily: change default channel to weekly

--- src/plugins/pixiv_daily.py
+++ src/plugins/pixiv_daily.py
@@ -2,7 +2,7 @@ from nonebot import on_command
 from nonebot.adapters import Bot
 from nonebot.typing import T_State
 from nonebot.adapters.cqhttp import MessageEvent, MessageSegment
-from nonebot.permission import SUPERUSER
+# from nonebot.permission import SUPERUSER
 
 import feedparser
 import re
@@ -15,9 +15,30 @@ from urllib.parse import quote
 socket.setdefaulttimeout(5)
 
 
-pixiv_search = on_command("test给点", permission=SUPERUSER, priority=1, block=True)
+# pixiv_search = on_command("test给点", permission=SUPERUSER, priority=1, block=True)
 
-@pixiv_search.handle()
+# @pixiv_search.handle()
+# async def handle(bot: Bot, event: MessageEvent, state: T_State):
+#     raw_args = str(event.get_message()).strip()
+#     if raw_args:
+#         arg_list = raw_args.split()
+#         argc = len(arg_list)
+#         for i in range(argc):
+#             state[f"arg{i+1}"] = arg_list[i]
+
+#     await pixiv_search.send("正在搜索[" + state["arg1"] + "]，请耐心等待...")
+
+#     is_timeout, status, data = await get_image_data(keyword=state["arg1"])
+#     if not is_timeout:
+#         chosen = choice(data)
+#         await pixiv_search.finish(f"{chosen[0]}\n{chosen[1]}\n" + MessageSegment.image(chosen[2]))
+#     else:
+#         await pixiv_search.finish("苦しい……请求超时了，稍后重试一下呢")
+
+
+pixiv_daily = on_command("给点", priority=1, block=True)
+
+@pixiv_daily.handle()
 async def handle(bot: Bot, event: MessageEvent, state: T_State):
     raw_args = str(event.get_message()).strip()
     if raw_args:
@@ -26,23 +47,16 @@ async def handle(bot: Bot, event: MessageEvent, state: T_State):
         for i in range(argc):
             state[f"arg{i+1}"] = arg_list[i]
 
-    await pixiv_search.send("正在搜索[" + state["arg1"] + "]，请耐心等待...")
+    if state["arg1"] == "日榜":
+        is_timeout, status, data = await get_image_data(url="https://rakuen.thec.me/PixivRss/daily-30")
+    else:
+        is_timeout, status, data = await get_image_data(url="https://rakuen.thec.me/PixivRss/weekly-30")
 
-    is_timeout, status, data = await get_image_data(keyword=state["arg1"])
     if not is_timeout:
         chosen = choice(data)
-        await pixiv_search.finish(f"{chosen[0]}\n{chosen[1]}\n" + MessageSegment.image(chosen[2]))
+        await pixiv_daily.finish(f"{chosen[0]}\n{chosen[1]}\n" + MessageSegment.image(chosen[2]))
     else:
-        await pixiv_search.finish("苦しい……请求超时了，稍后重试一下呢")
-
-
-pixiv_daily = on_command("给点", priority=1, block=True)
-
-@pixiv_daily.handle()
-async def handle(bot: Bot, event: MessageEvent):
-    is_timeout, status, data = await get_image_data(url="https://rakuen.thec.me/PixivRss/daily-30")
-    chosen = choice(data)
-    await pixiv_daily.finish(f"{chosen[0]}\n{chosen[1]}\n" + MessageSegment.image(chosen[2]))
+        await pixiv_daily.finish("苦しい……请求超时了，稍后重试一下呢")
 
 
 async def get_image_data(url: str = None, keyword: str = None) -> (bool, str, list):
@@ -62,9 +76,7 @@ async def get_image_data(url: str = None, keyword: str = None) -> (bool, str, li
     else:
         for entry in rss["entries"]:
             data.append([entry["title"], entry["link"], entry["summary"]])
-
         for item in data:
             item[1] = item[1][12:]
             item[2] = re.findall(r"src=.+?jpg", item[2])[0][5:]
-
         return (False, rss["status"], data)

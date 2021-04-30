899965ae9c9b9505bc0f3bd46ff39023135c859d
Purple4pur <purple4pur@gmail.com>
2021-04-30 16:35:54 +0800

    test

--- src/plugins/auto_handle_notice.py
+++ src/plugins/auto_handle_notice.py
@@ -4,15 +4,20 @@ from nonebot.adapters import Bot
 from nonebot.adapters.cqhttp.event import FriendRequestEvent, FriendAddNoticeEvent, FriendRecallNoticeEvent
 
 
-friend_add = on_notice()
+# friend_add = on_notice()
 
-@friend_add.handle()
-async def handle(bot: Bot, event: FriendAddNoticeEvent):
-    print(event)
+# @friend_add.handle()
+# async def handle(bot: Bot, event: FriendRequestEvent):
+#     await event.approve(bot)
 
 
 friend_recall = on_notice()
 
 @friend_recall.handle()
 async def handle(bot: Bot, event: FriendRecallNoticeEvent):
-    print(event)
+    print(event.message_id)
+    msg_id = event.message_id
+    msg = await bot.get_msg(message_id=msg_id)
+    print(msg)
+    await bot.send(event, msg)
+

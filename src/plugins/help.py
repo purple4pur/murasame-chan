0b66013b71949d1d7284452d77250d1a8a0e48ac
Purple4pur <purple4pur@gmail.com>
2021-05-13 17:13:02 +0800

    fix help.py

--- src/plugins/help.py
+++ src/plugins/help.py
@@ -4,6 +4,6 @@ from nonebot.adapters import Bot
 
 help_menu = on_command("help", aliases={"帮助"}, priority=1, block=True)
 
-@help.handle()
+@help_menu.handle()
 async def handle(bot: Bot):
     await help_menu.finish("前往 purple4pur.com/murasame-chan 查看帮助哦！")

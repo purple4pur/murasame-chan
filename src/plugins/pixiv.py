41789a10bfb8a90ad3fdb7fb43fe8408b1346c1f
Purple4pur <purple4pur@gmail.com>
2021-05-15 20:57:23 +0800

    处理 httpx.ConnectError 异常

--- src/plugins/pixiv.py
+++ src/plugins/pixiv.py
@@ -7,7 +7,7 @@ import re
 from random import choice
 from urllib.parse import quote
 from feedparser_data import RssAsync
-from httpx import ConnectTimeout
+from httpx import ConnectTimeout, ConnectError
 
 
 pixiv = on_command("给点", priority=1, block=True)
@@ -23,25 +23,27 @@ async def handle(bot: Bot, event: MessageEvent, state: T_State):
             state[f"arg{i+1}"] = arg_list[i]
 
     if argc > 0 and state["arg1"] == "日榜":
-        is_timeout, data = await get_image_data(url="https://rakuen.thec.me/PixivRss/daily-20")
+        is_timeout, is_error, data = await get_image_data(url="https://rakuen.thec.me/PixivRss/daily-20")
     elif argc > 0:
         keyword = state["arg1"]
         await pixiv.send(f"正在查询[{keyword}]……")
-        is_timeout, data = await get_image_data(keyword=keyword)
+        is_timeout, is_error, data = await get_image_data(keyword=keyword)
     else:
-        is_timeout, data = await get_image_data(url="https://rakuen.thec.me/PixivRss/weekly-30")
+        is_timeout, is_error, data = await get_image_data(url="https://rakuen.thec.me/PixivRss/weekly-30")
 
     if is_timeout:
-        await pixiv.finish("苦しい……请求超时了，稍后重试一下呢")
+        await pixiv.finish("苦しい……请求超时了(´。＿。｀)")
+    if is_error:
+        await pixiv.finish("苦しい……连接出错了(´。＿。｀)")
     elif len(data) == 0:
-        await pixiv.finish("寂しい……什么都没有呢")
+        await pixiv.finish("寂しい……什么都没找到呢")
     else:
         chosen = choice(data)
         await pixiv.finish(f"{chosen[0]}\n{chosen[1]}\n" + MessageSegment.image(chosen[2]))
 
 
-async def get_image_data(url: str = None, keyword: str = None) -> (bool, list):
-                                                             # 是否超时，data 数组
+async def get_image_data(url: str = None, keyword: str = None) -> (bool, bool, list):
+                                                         # 是否超时，是否连接出错，data 数组
     data = []
 
     if keyword:
@@ -53,7 +55,10 @@ async def get_image_data(url: str = None, keyword: str = None) -> (bool, list):
 
     # 连接超时
     except ConnectTimeout:
-        return (True, data)
+        return (True, False, data)
+    # 连接出错
+    except ConnectError:
+        return (False, True, data)
 
     else:
         for entry in rss["entries"]:
@@ -61,4 +66,4 @@ async def get_image_data(url: str = None, keyword: str = None) -> (bool, list):
         for item in data:
             item[1] = item[1][12:].replace("artworks", "i")
             item[2] = re.findall(r"src=.+?jpg", item[2])[0][5:]
-        return (False, data)
+        return (False, False, data)

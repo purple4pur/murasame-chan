e20f9e3e57f0dfa034e2062f68b1bbb2dba905c5
Purple4pur <purple4pur@gmail.com>
2021-06-25 12:40:05 +0800

    测试 t.ly 短链发图的可用性 2

--- src/plugins/test.py
+++ src/plugins/test.py
@@ -8,3 +8,10 @@ test = on_command("test", priority=1, block=True)
 @test.handle()
 async def handle(bot: Bot, event: MessageEvent, state: T_State):
     await test.finish(MessageSegment.image("https://t.ly/Syi3"))
+
+
+test2 = on_command("test2", priority=1, block=True)
+
+@test2.handle()
+async def handle(bot: Bot, event: MessageEvent, state: T_State):
+    await test2.finish(MessageSegment.image("https://t.ly/lciV"))
